16cc20b491e7641d0e9dfdf2ef5541c833b13f46
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-06-22 20:32:41 +0530

    employee P/R

--- emp.sh
+++ emp.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo " eployee is present or absent using RANDOM keywords "
+
+ispresent=5 ;
+randomcheck=$((RANDOM%6)) ;
+
+if [ $ispresent = $randomcheck ]
+then
+echo " emp is PRESENT "
+else
+echo  " emp is ABSENT "
+fi
