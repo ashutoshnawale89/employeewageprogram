b16f1532bd48791412bf716bf2d179fd348c0cf1
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-06-22 20:51:53 +0530

    emp wage salary

---
+++ emp2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+ishalfday=1;
+isfullday=3;
+empratehr=20;
+
+empcheck=$((RANDOM%4));
+
+case $empcheck in
+$ishalfday)
+                  emphrs=4;;
+$isfullday)
+                   emphrs=8;;
+*)
+                  emphrs=0;;
+esac
+salary=$(($emphrs * $empratehr));
+
+echo "salary of emply $salary" 
